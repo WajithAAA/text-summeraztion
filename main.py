08ba236cc3128fba560f9cdee5374d26fd67b5be
WajithAAA <azeeswajid@gmail.com>
2023-06-17 22:44:51 +0530

    data validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from TextSummarizer.logging import logger
-from TextSummarizer.pipelines.stege_01_data_ingestion import DataIngestionTrainingPipeline
+from TextSummarizer.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TextSummarizer.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = 'DATA_INGESTION_STAGE'
@@ -15,4 +16,15 @@ except Exception as e:
     raise e
 
 
+STAGE_NAME = 'DATA_VALIDATION_STAGE'
+
+try:
+    logger.info(f'>>>>>> stage %s started <<<<<<<' % STAGE_NAME)
+    data_Validation = DataValidationTrainingPipeline()
+    data_Validation.main()
+    logger.info(f'>>>>>> stage %s completed <<<<<<<' % STAGE_NAME)
+
+except Exception as e:
+    logger.exception(e)
+    raise e
 
